b6d4a843fedc4ae63c74097a463cd4896f2445cd
Andreea <suntandreea@protonmail.com>
2025-03-05 15:52:54 +0200

    Get player choice function

--- script.js
+++ script.js
@@ -9,4 +9,15 @@ function getComputerChoice() {
   return 2;
 }
 
+function getPlayerChoice() {
+  const input = prompt(`Pick your choice. Insert 0 for "ROCK", 1 for "PAPER" and 2 for "SCISSORS".`);
+  if (input === "0") return 0;
+  if (input === "1") return 1;
+  return 2;
+}
+
 const computer = getComputerChoice();
+const player = getPlayerChoice();
+
+log("Computer picked ", choices[computer], ".");
+log("You picked ", choices[player], ".");
