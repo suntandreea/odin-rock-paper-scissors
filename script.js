198ffa6f354f6362515e243d69e35fac858571b5
Andreea <suntandreea@protonmail.com>
2025-03-05 15:50:03 +0200

    Start rock-paper-scissors game logic
    Create an array to hold all game possible choices,
    implement a function to get the computer's choice
    and store it inside a variable.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 const log = console.log;
+
+const choices = ["ROCK", "PAPER", "SCISSORS"];
+
+function getComputerChoice() {
+  const choice = Math.floor(Math.random() * 100);
+  if (choice < 33) return 0;
+  if (choice < 66) return 1;
+  return 2;
+}
+
+const computer = getComputerChoice();
